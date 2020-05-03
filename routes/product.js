fb9d42248d995042bb90a08c9676eb380f4e9d12
Jonathan <jonathang@hotmail.com.ar>
2020-05-03 19:02:53 -0300

    Agregando Mliebrev3crud

---
+++ routes/product.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const productsController = require('../controllers/productsController')
+
+router.get('/add',productsController.create);
+router.get('/edit/:id', productsController.edit);
+router.put('/edit/:id', productsController.update);
+router.get('/ok', productsController.modificado);
+
+
+
+
+
+
+
+router.get('/detail/:id/:category',productsController.index)
+
+
+
+module.exports = router;
